a17ad699a6e23a18f6ea6887ba46f5d03bdc6d5a
e <892459897@qq.com>
2021-01-15 14:29:46 +0800

    代码格式化

--- src/leetcode/algorithm/IsPalindrome.java
+++ src/leetcode/algorithm/IsPalindrome.java
@@ -1,30 +1,30 @@
 package leetcode.algorithm;
 
 public class IsPalindrome {
-	public static boolean isPalindrome(int x) {
-		if(x==0){
+    public static boolean isPalindrome(int x) {
+        if (x == 0) {
             return true;
-        }else if(x<0){
+        } else if (x < 0) {
             return false;
         }
-        int length = (int)Math.log10(x)+1;
-        System.out.print("length="+length+"\n");
-        if(length==2){
-            int high = (x%100)/10;
-        	int low = (x%10);
-            if(high==low){
+        int length = (int) Math.log10(x) + 1;
+        System.out.print("length=" + length + "\n");
+        if (length == 2) {
+            int high = (x % 100) / 10;
+            int low = (x % 10);
+            if (high == low) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
         }
-        for(int i=1,j=length;i!=j & i-j!=1;i++,j--){
-        	System.out.print(""+i+""+"\n");
-        	int high = (x%((int) Math.pow(10,j)))/((int) Math.pow(10,j-1));
-        	int low = (x%((int) Math.pow(10,i)))/((int) Math.pow(10,i-1));
-        	System.out.print("high="+high+"\n");
-        	System.out.print("low="+low+"\n");
-            if(high!=low){
+        for (int i = 1, j = length; i != j & i - j != 1; i++, j--) {
+            System.out.print("" + i + "" + "\n");
+            int high = (x % ((int) Math.pow(10, j))) / ((int) Math.pow(10, j - 1));
+            int low = (x % ((int) Math.pow(10, i))) / ((int) Math.pow(10, i - 1));
+            System.out.print("high=" + high + "\n");
+            System.out.print("low=" + low + "\n");
+            if (high != low) {
                 return false;
             }
 
