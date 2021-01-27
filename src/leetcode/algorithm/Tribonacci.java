c39d92199dff873b2949e7f711de3e8c52510bd5
e <892459897@qq.com>
2021-01-27 19:19:33 +0800

    删除乱码

--- src/leetcode/algorithm/Tribonacci.java
+++ src/leetcode/algorithm/Tribonacci.java
@@ -2,9 +2,9 @@ package leetcode.algorithm;
 
 public class Tribonacci {
     /**
-     *泰波那契序列 Tn 定义如下： 
-     * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
-     * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
+     *泰波那契序列 Tn 定义如下：
+     * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
+     * 给你整数 n，请返回第 n 个泰波那契数 Tn 的值。
      */
     public static int tribonacci(int n) {
 
