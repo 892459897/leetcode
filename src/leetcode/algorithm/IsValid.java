825448c24e9cd865835789fae07dcf0264ab8a53
892459897 <892459897@qq.com>
2021-10-19 21:02:04 +0800

    添加题目说明

--- src/leetcode/algorithm/IsValid.java
+++ src/leetcode/algorithm/IsValid.java
@@ -4,6 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 class IsValid {
+    /**
+     * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     *
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     *
+     */
+
+
     private static final Map<Character,Character> map = new HashMap<Character,Character>(){{
         put('{','}'); put('[',']'); put('(',')'); put('?','?');
     }};
--- src/leetcode/algorithm/MergeTwoLists.java
+++ src/leetcode/algorithm/MergeTwoLists.java
@@ -1,6 +1,9 @@
 package leetcode.algorithm;
 
 public class MergeTwoLists {
+    /**
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     */
 
 
      public class ListNode {
